317ff582be47adce1b8ec793c4678c4807facf09
suquiya <22910980+suquiya@users.noreply.github.com>
2020-09-25 22:05:38 +0900

    hookの実装を進める

--- src/hook.rs
+++ src/hook.rs
@@ -66,16 +66,73 @@ impl Hook {
 			alias,
 		}
 	}
+}
 
-	/// Sets action
-	pub fn action(mut self, action: HookAction) -> Self {
-		self.action = Some(action);
+macro_rules! gen_doc {
+	($field:ident) => {
+		concat!("Sets ", stringify!($field), ".")
+	};
+}
+
+macro_rules! setter {
+	($field:ident)=>{
+		setter!($field, gen_doc!($field));
+	};
+	($field:ident, $fn_doc:expr) => {
+		#[doc=$fn_doc]
+		pub fn $field<T: Into<String>>(mut self, $field: T) -> Self {
+			self.$field = $field.into();
+			self
+		}
+	};
+	($field:ident :Option String)=>{
+		setter!($field,gen_doc!($field));
+	};
+	($field:ident :Option String, $fn_doc:expr)=>{
+		#[doc=$fn_doc]
+		pub fn $field<T:Into<String>>(mut self, $field: T)->Self{
+			self.$field = Some($field.into());
+			self
+		}
+	};
+	($field:ident :Option $t:ty)=>{
+		setter!($field :Option $t, gen_doc!($field));
+	};
+	($field:ident :Option $t:ty, $fn_doc:expr)=>{
+		#[doc=$fn_doc]
+		pub fn $field(mut self, $field: $t)->Self{
+			self.$field = Some($field);
+			self
+		}
+	};
+	($($field:ident),+)=>{
+		$(setter!($field);)+
+	};
+	($($field:ident :Option $t:ty),+)=>{
+		$(setter!($field :Option $t);)+
+	}
+}
+
+impl Hook {
+	setter!(name, authors, copyright, usage);
+	setter!(action:Option HookAction,license: Option (String, String),description:Option String);
+
+	/// Add alias to this hook.
+	pub fn alias<T: Into<String>>(mut self, alias: T) -> Self {
+		self.alias.push(alias.into());
 		self
 	}
 
-	/// Sets name
-	pub fn name<T: Into<String>>(mut self, name: T) -> Self {
-		self.name = name.into();
-		self
+	/// Beturns true if name_or_alias matches command's name or one of alias at leaast.
+	/// name_or_aliasがコマンド名がエイリアスのうち少なくとも一つにマッチした場合trueを返す
+	pub fn is(&self, name_or_alias: &str) -> bool {
+		if name_or_alias == self.name {
+			true
+		} else {
+			match self.alias.inner() {
+				None => false,
+				Some(inner) => inner.iter().any(|a| a == name_or_alias),
+			}
+		}
 	}
 }
